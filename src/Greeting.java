d53053cceea680639156d156fdccc0f0f1c50460
Kelly Premchuen <virawan40@hotmail.com>
2017-12-08 11:42:27 +0100

    greeting

---
+++ src/Greeting.java
@@ -0,0 +1,13 @@
+public class Greeting {
+
+//    private String morning;
+//    private String night;
+    
+    public String morning (){
+        return "good morning";
+    }
+
+    public String night() {
+        return "good night";
+    }
+}
