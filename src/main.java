ae84477c68be89b76f0a11aeacf7d2ce0a70b604
Kelly Premchuen <virawan40@hotmail.com>
2017-12-18 12:23:41 +0100

    SQL Connection Class

--- src/main.java
+++ src/main.java
@@ -1,9 +1,5 @@
 import javax.swing.*;
-import java.sql.*;
 
-/**
- * Dit is een voorbeeld Java toepassing waarin je verbinding maakt met een SQLServer database.
- */
 public class main {
     public static void main(String[] args) {
         //SQL Connection
